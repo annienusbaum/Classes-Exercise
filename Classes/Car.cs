b7b0743de5db2b5b6db84b049cd0a1f21e678953
Annie Nusbaum <annie.nusbaum.83@gmail.com>
2023-11-20 16:11:40 -0500

    finished exercise

---
+++ Classes/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Classes
+{
+
+    public class Car
+    {
+        // Properties with non-null default values in the constructor
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        // Constructor
+        public Car()
+        {
+            Make = "";
+            Model = "";
+            Year = 0;
+        }
+    }
+
+
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Classes
+﻿using System.Collections.Generic;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create a new instance of the Car class
+            Car myCar = new Car();
+
+            // Set values in the properties for the object
+            myCar.Make = "Toyota";
+            myCar.Model = "Camry";
+            myCar.Year = 2022;
+
+            // Print the values of each property to the Console
+            Console.WriteLine("Car Make: " + myCar.Make);
+            Console.WriteLine("Car Model: " + myCar.Model);
+            Console.WriteLine("Car Year: " + myCar.Year);
         }
     }
 }
+
+
+
+//## Exercise 1:
+
+//+Create a class named Car - make sure it is public
+
+//### In the Car class:
+//+Create a Make property of type string that is public
+//   +Create a Model property of type string that is public
+// +Create a Year property of type int that is public
+//   +Create a new instance of the Car class
+//   + Print the values of each property to the Console
